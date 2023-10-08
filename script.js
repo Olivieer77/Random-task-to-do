a504c992f21a65e41af4c4ec1ec114814c6b375d
Olivier <olivier.smialkowski@gmail.com>
2023-10-08 19:36:44 +0200

    R4 more structured code

--- script.js
+++ script.js
@@ -45,60 +45,91 @@ const tasks = {
 
 const customTasks = []
 
+const taskDisplay = document.querySelector(".task");
+const categoryButtons = document.querySelectorAll(".category-button");
+const expandCategory = document.querySelector('.category-expand');
+const customTaskInput = document.getElementById("customTaskInput");
+const customTaskList = document.getElementById('customTaskList');
+const addCustomTaskButton = document.getElementById("addCustomTask");
+const AddError = document.querySelector('.Add-error');
+const shareButton = document.getElementById("shareButton");
+
 function generateRandomTask(category = "all") {
     const categoryTasks = tasks[category];
     if (!categoryTasks) return;
 
     const randomIndex = Math.floor(Math.random() * categoryTasks.length);
-    const randomTask = categoryTasks[randomIndex];
-    document.querySelector(".task").textContent = randomTask;
+    taskDisplay.textContent = categoryTasks[randomIndex];
 }
 
-const categoryButtons = document.querySelectorAll(".category-button");
 categoryButtons.forEach(button => {
     button.addEventListener("click", event => {
         const selectedCategory = event.target.getAttribute("data-category");
         generateRandomTask(selectedCategory);
-    })
+    });
 });
 
-generateRandomTask(); 
+generateRandomTask();
 
-const expandCategory = document.querySelector('.category-expand')
 expandCategory.addEventListener('click', () => {
     categoryButtons.forEach(e => {
-        e.classList.toggle('visible')
-    })
-})
-
-
-const customTaskInput = document.getElementById("customTaskInput");
-const customTaskList = document.getElementById('customTaskList');
-const addCustomTaskButton = document.getElementById("addCustomTask");
+        e.classList.toggle('visible');
+    });
+});
 
 function addCustomTaskToList(taskText) {
     const customTaskElement = document.createElement('li');
     customTaskElement.textContent = taskText;
 
-    const deleteIcon = document.createElement("span");
-    deleteIcon.textContent = "🗑️"; 
-    deleteIcon.className = "delete-icon";
+    function createIcon(iconText, iconClass, clickHandler) {
+        const icon = document.createElement("span");
+        icon.textContent = iconText;
+        icon.className = iconClass;
+        icon.addEventListener("click", clickHandler);
+        return icon;
+    }
 
-    deleteIcon.addEventListener("click", () => {
-        deleteCustomTask(taskText);
-        customTaskElement.remove();
-    });
+    function deleteCustomTask() {
+        const taskIndex = customTasks.indexOf(taskText);
+        if (taskIndex !== -1) {
+            customTasks.splice(taskIndex, 1);
+            customTaskElement.remove();
+        }
+    }
 
-    const editIcon = document.createElement("span");
-    editIcon.textContent = "✏️"; 
-    editIcon.className = "edit-icon";
-    editIcon.addEventListener("click", () => {
+    function editCustomTask(taskText, listItem) {
+        const newTaskText = prompt("Edytuj zadanie:", taskText);
+        if (newTaskText !== null) {
+            const taskIndex = customTasks.indexOf(taskText);
+            if (taskIndex !== -1) {
+                customTasks[taskIndex] = newTaskText;
+                listItem.textContent = newTaskText;
+
+                let deleteIcon = listItem.querySelector(".delete-icon");
+                let editIcon = listItem.querySelector(".edit-icon");
+
+                if (!deleteIcon) {
+                    deleteIcon = createIcon("🗑️", "delete-icon", () => {
+                        deleteCustomTask(newTaskText);
+                        listItem.remove();
+                    });
+                    listItem.appendChild(deleteIcon);
+                }
+
+                if (!editIcon) {
+                    editIcon = createIcon("✏️", "edit-icon", () => {
+                        editCustomTask(newTaskText, listItem);
+                    });
+                    listItem.appendChild(editIcon);
+                }
+            }
+        }
+    }
+
+    customTaskElement.appendChild(createIcon("🗑️", "delete-icon", deleteCustomTask));
+    customTaskElement.appendChild(createIcon("✏️", "edit-icon", () => {
         editCustomTask(taskText, customTaskElement);
-    });
-
-    customTaskElement.appendChild(deleteIcon);
-    customTaskElement.appendChild(editIcon);
-
+    }));
     customTaskList.appendChild(customTaskElement);
 }
 
@@ -109,72 +140,8 @@ function deleteCustomTask(taskText) {
     }
 }
 
-function editCustomTask(oldTaskText, listItem) {
-    const newTaskText = prompt("Edytuj zadanie:", oldTaskText);
-    if (newTaskText !== null) {
-        const taskIndex = customTasks.indexOf(oldTaskText);
-        if (taskIndex !== -1) {
-            customTasks[taskIndex] = newTaskText;
-            listItem.textContent = newTaskText;
-            
-              // Zaktualizuj tekst w spanie
-              listItem.textContent = newTaskText;
-
-            // Sprawdź, czy istnieją ikony edycji i usuwania
-            let deleteIcon = listItem.querySelector(".delete-icon");
-            let editIcon = listItem.querySelector(".edit-icon");
-
-            if (!deleteIcon) {
-                deleteIcon = document.createElement("span");
-                deleteIcon.textContent = "🗑️"; 
-                deleteIcon.className = "delete-icon";
-                deleteIcon.addEventListener("click", () => {
-                    deleteCustomTask(newTaskText);
-                    listItem.remove();
-                });
-                listItem.appendChild(deleteIcon);
-            }
-
-            if (!editIcon) {
-                editIcon = document.createElement("span");
-                editIcon.textContent = "✏️"; 
-                editIcon.className = "edit-icon";
-                editIcon.addEventListener("click", () => {
-                    editCustomTask(newTaskText, listItem);
-                });
-                listItem.appendChild(editIcon);
-            }
-        }
-    }
-}
-
-const AddError = document.querySelector('.Add-error')
-addCustomTaskButton.addEventListener('click', () => {
-    const taskText = customTaskInput.value.trim();
-    if (taskText && !customTasks.includes(taskText)) {
-        customTasks.push(taskText);
-        addCustomTaskToList(taskText);
-        customTaskInput.value = "";
-        AddError.textContent = '';
-    } else {
-        AddError.textContent = 'Error insert task name';
-    }
-    
-});
-
-customTaskList.addEventListener("click", (event) => {
-    if (event.target.tagName === "SPAN" && event.target.className === "delete-icon") {
-        const taskText = event.target.parentElement.textContent;
-        const taskIndex = customTasks.indexOf(taskText);
-        if (taskIndex !== -1) {
-            customTasks.splice(taskIndex, 1);
-            event.target.parentElement.remove();
-        }
-    }
-});
-
-
-function shareTask(taskText) {
+function shareCurrentTask() {
+    const taskText = taskDisplay.textContent;
     if (navigator.share) {
         navigator.share({
             title: 'Random Task to do in your free time',
@@ -187,8 +154,16 @@ function shareTask(taskText) {
     }
 }
 
-const shareButton = document.getElementById("shareButton");
-shareButton.addEventListener('click', () => {
-    const taskText = document.querySelector(".task").textContent;
-    shareTask(taskText);
+addCustomTaskButton.addEventListener('click', () => {
+    const taskText = customTaskInput.value.trim();
+    if (taskText && !customTasks.includes(taskText)) {
+        customTasks.push(taskText);
+        addCustomTaskToList(taskText);
+        customTaskInput.value = "";
+        AddError.textContent = '';
+    } else {
+        AddError.textContent = 'Error insert task name';
+    }
 });
+
+shareButton.addEventListener('click', shareCurrentTask);
