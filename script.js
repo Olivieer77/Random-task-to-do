b547782999f7970184301ba29e1eb6d80e93768b
Olivier <olivier.smialkowski@gmail.com>
2023-10-21 19:59:00 +0200

    R10 Visual changes and fixes

--- script.js
+++ script.js
@@ -1,47 +1,94 @@
 const tasks = {
     all: [
-    "train for 1 hour",
-    "stretch for 30 minutes",
-    "go to the gym",
-    "Idź na spacer przez 30 minut.",
-    "train for 1 hour",
-    "stretch for 30 minutes",
-    "Idź na spacer przez 30 minut.",
-    "medytuj",
-    "napisz za co jesteś wdzięczny",
-    "Zadzwoń do przyjaciela i porozmawiaj przez telefon.",
-    "Read a book for 30 minutes",
-    "Nauka obcego języka",
-    "Znajdź nowy przepis i przygotuj nowe danie.",
+        "train for 1 hour.",
+        "stretch for 30 minutes.",
+        "Visit the gym.",
+        "Bike for an hour.",
+        "Do 30 minutes of cardio exercises.",
+        "Run for 30 minutes.",
+        "Go to the pool.",
+        "Walk for 30 minut.",
+        "Hold a plank for 2 minutes.",
+        "Visit a squash court.",
+        "Draw for 30 minutes.",
+        "Posprzątaj swój pokój.",
+        "Photograph nature.",
+        "Graphic design.",
+        "Keep a record of your ideas.",
+        "Meditate for 20 minutes.",
+        "Read a book for 30 minutes.",
+        "Write down what you are grateful for.",
+        "Take a walk in the park.",
+        "Watch a film.",
+        "Listen to music.",
+        "Clean your room.",
+        "Play with your dog outdoors.",
+        "Learn a new language.",
+        "Study cooking basics.",
+        "Learn to play a musical instrument.",
+        "Learn coding.",
+        "Try new technology.",
+        "Enroll in an online course.",
+        // 
+        "Try a new cooking recipe.",
+        "Make a list of your goals.",
+        "Visit an art exhibition.",
+        "Listen to your favorite music.",
+        "Buy fresh fruits and vegetables.",
+        "Invite friends over for dinner.",
+
     ],
 
      fitness:  [
-        "train for 1 hour",
-        "stretch for 30 minutes",
-        "go to the gym",
-        "Idź na spacer przez 30 minut.",
+        "train for 1 hour.",
+        "stretch for 30 minutes.",
+        "Visit the gym.",
+        "Bike for an hour.",
+        "Do 30 minutes of cardio exercises.",
+        "Run for 30 minutes.",
+        "Go to the pool.",
+        "Walk for 30 minut.",
+        "Hold a plank for 2 minutes.",
+        "Visit a squash court.",
     ],
     
      creativity:  [
-        "zapisuj pomysly jakies",
-        "rób jakieś inne głoptki",
-        "Napisz dziennik przez 15 minut.",
+        "Draw for 30 minutes.",
         "Posprzątaj swój pokój.",
-    
+        "Photograph nature.",
+        "Graphic design.",
+        "Keep a record of your ideas.",
     ],
     
-     rest:  [
-        "medytuj",
-        "napisz za co jesteś wdzięczny",
-        "Zadzwoń do przyjaciela i porozmawiaj przez telefon.",
-        "Read a book for 30 minutes",
+    relax:  [
+        "Meditate for 20 minutes.",
+        "Read a book for 30 minutes.",
+        "Write down what you are grateful for.",
+        "Take a walk in the park.",
+        "Watch a film.",
+        "Listen to music.",
+        "Clean your room.",
+        "Play with your dog outdoors."
     ],
     
      study:  [
-        "Nauka obcego języka",
-        "Znajdź nowy przepis i przygotuj nowe danie.",
+        "Learn a new language.",
+        "Study cooking basics.",
+        "Learn to play a musical instrument.",
+        "Learn coding.",
+        "Try new technology.",
+        "Enroll in an online course.",
+
     ],
 }
+
+const completedTasks = {
+    all: [],
+    fitness: [],
+    creativity: [],
+    relax: [],
+    study: [],
+};
 
 const taskDisplay = document.querySelector(".task");
 const categoryButtons = document.querySelectorAll(".category-button");
@@ -50,10 +97,25 @@ const shareButton = document.getElementById("shareButton");
 
 function generateRandomTask(category = "all") {
     const categoryTasks = tasks[category];
-    if (!categoryTasks) return;
+    if (!categoryTasks || categoryTasks.length === 0) {
+        alert("No tasks available in this category.");
+        return;
+    }
 
     const randomIndex = Math.floor(Math.random() * categoryTasks.length);
-    taskDisplay.textContent = categoryTasks[randomIndex];
+    const task = categoryTasks[randomIndex];
+    categoryTasks.splice(randomIndex, 1); 
+    completedTasks[category].push(task); 
+
+    taskDisplay.textContent = task;
+    // taskDisplay.textContent = categoryTasks[randomIndex];
+}
+
+function resetCategoryTasks() {
+    for (const category in tasks) {
+        tasks[category] = completedTasks[category].concat(tasks[category]);
+        completedTasks[category] = [];
+    }
 }
 
 categoryButtons.forEach(button => {
@@ -72,6 +134,45 @@ expandCategory.addEventListener('click', () => {
     expandCategory.classList.toggle('expanded')
 });
 
+
+
+const completedRandomTasks = [];
+const completedRandomTaskHistory = [];
+
+function completeRandomTask(){
+    const taskText = taskDisplay.textContent;
+    if (!completedRandomTasks.includes(taskText)) {
+        completedRandomTasks.push(taskText);
+        completedRandomTaskHistory.push(taskText);
+        updateCompletedCount();
+        displayCompletedRandomTaskHistory();
+        generateRandomTask();
+    }
+}
+
+function updateCompletedCount() {
+    const completedCountElement = document.getElementById("completedCount");
+    completedCountElement.textContent = completedRandomTasks.length;
+}
+
+function displayCompletedRandomTaskHistory() {
+    const completedRandomTaskHistoryList = document.querySelector(".history");
+    completedRandomTaskHistoryList.innerHTML = "";
+
+    for (const completedTask of completedRandomTaskHistory) {
+        const historyItem = document.createElement("li");
+        historyItem.textContent = completedTask;
+        completedRandomTaskHistoryList.appendChild(historyItem);
+    }
+}
+
+
+taskDisplay.addEventListener('click', completeRandomTask);
+
+
+
+
+
 // udostępnianie
 function shareCurrentTask() {
     const taskText = taskDisplay.textContent;
@@ -86,7 +187,5 @@ function shareCurrentTask() {
         alert('Twoja przeglądarka nie obsługuje funkcji udostępniania.');
     }
 }
-
-
 
 shareButton.addEventListener('click', shareCurrentTask);
